dc7879c0b3a39158471144d813557d800f4f20f3
Семён Голубев <41947359+SemkaHub@users.noreply.github.com>
2022-11-11 18:27:32 +1200

    initialize objects in MainActivity.kt

--- app/src/main/java/com/example/android/notes/MainActivity.kt
+++ app/src/main/java/com/example/android/notes/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.android.notes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.android.notes.databinding.ActivityMainBinding
+import com.example.android.notes.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+        mToolbar = mBinding.toolbar
+        mNavController = mBinding.navHostFragment.getFragment<NavHostFragment>().navController
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.title)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/android/notes/utilits/constants.kt
@@ -0,0 +1,5 @@
+package com.example.android.notes.utilits
+
+import com.example.android.notes.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
